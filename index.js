e99ef02cd75869ae9a4e18e703d3e050eb04ce49
sixtus iwuchukwu <sixtusiwuchukwu21@gmail.com>
2021-08-26 02:46:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const http = require("http");
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const { ApolloServer, PubSub } = require("apollo-server-express");
+
+const { typeDefs, resolvers } = require("./src/schema");
+
+const datasources = require("./src/datasource");
+
+// initialize app
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-with, Content-Type,Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT,POST,GET,DELETE,PATCH,UPDATE"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// connect to database
+
+
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  subscriptions: {
+    onConnect: () => console.log("websocket connected"),
+    onDisconnect: () => console.log("websocket disconnected"),
+  },
+  context: ({ req, res }) => ({
+    req,
+    res,
+    pubsub,
+    datasources,
+    engine: {
+      reportSchema: true,
+    },
+  }),
+  introspection: true,
+  tracing: true,
+  playground: true,
+});
+// setting middleware
+
+server.applyMiddleware({ app, path: "/" });
+
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+// initialize server port
+const PORT = process.env.PORT || 2000;
+
+
+httpServer.listen(PORT, () =>
+  console.log(`🚀 Server is ready at port ${PORT}`)
+ 
+);
+
+console.log(
+  `subscription is ready at localhost:${PORT}${server.subscriptionsPath}`
+);
