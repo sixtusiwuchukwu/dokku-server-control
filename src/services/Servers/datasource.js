f5fa2fab2c09dec75b0fd4d5864496966386c956
sixtus iwuchukwu <sixtusiwuchukwu21@gmail.com>
2021-09-02 21:37:54 +0100

    setting up dokku manager

--- src/services/Servers/datasource.js
+++ src/services/Servers/datasource.js
@@ -1,41 +1,98 @@
 const __Server = require("../../models/servers/servers");
-const {UserInputError} = require('apollo-server-express')
-const Base = require('../../../base')
-class ServerDatasource extends Base{
-  async listServers({search, page}) {
+const { UserInputError } = require("apollo-server-express");
+const Base,
+  { ssh } = require("../../../base");
+
+class ServerDatasource extends Base {
+  async listServers({ search, page }) {
     const query = {};
-    if ( search ) {
-      query[ 'serverName' ] = { $regex : search , $options : 'ig' }
+    if (search) {
+      query["serverName"] = { $regex: search, $options: "ig" };
     }
     const options = {
-      page : page ,
-      limit : 5 ,
-      projection: {pkey: 0, username: 0},
-      sort : { createdAt : -1 } ,
-      collation : {
-        locale : 'en'
-      }
+      page: page,
+      limit: 5,
+      projection: { pkey: 0, username: 0 },
+      sort: { createdAt: -1 },
+      collation: {
+        locale: "en",
+      },
     };
-  
-    return await __Server.paginate( {...query} , options )
+
+    return await __Server.paginate({ ...query }, options);
   }
   async addServer(data) {
     try {
-      const {username, host, pkey} = data
-      await this.RemoteServer(host, username, pkey)
-      await __Server.create(data)
-      return "New server Added"
-    }
-    catch (e) {
-      if(e.message.includes('authentication methods failed')) {
-        throw new UserInputError(`Cannot verify server credentials for ${data.host}`)
+      const { username, host, pkey } = data;
+      await this.RemoteServer(host, username, pkey);
+      await __Server.create(data);
+      return "New server Added";
+    } catch (e) {
+      if (e.message.includes("authentication methods failed")) {
+        throw new UserInputError(
+          `Cannot verify server credentials for ${data.host}`
+        );
       }
-      if (e.name === "Error" || e.name === "TypeError"|| e.name === "ReferenceError") {
+      if (
+        e.name === "Error" ||
+        e.name === "TypeError" ||
+        e.name === "ReferenceError"
+      ) {
         console.log(e);
-        throw new UserInputError("Unable to complete server setup contact support");
+        throw new UserInputError(
+          "Unable to complete server setup contact support"
+        );
       }
       throw new UserInputError(e.message);
     }
+  }
+  async StopServer(data) {
+    try {
+      const { _id } = data;
+
+      let FoundServer = await __Server.findOne({ _id });
+
+      if (!FoundServer) {
+        throw new UserInputError("Unable to stop server");
+      }
+      const { host } = FoundServer;
+
+      await this.RemoteServer(host, username, pkey);
+
+      await ssh.execCommand(`dokku ps:stop${host}`);
+    } catch (e) {
+      if (e.message.includes("authentication methods failed")) {
+        throw new UserInputError(
+          `Cannot verify server credentials for ${data.host}`
+        );
+      }
+      if (
+        e.name === "Error" ||
+        e.name === "TypeError" ||
+        e.name === "ReferenceError"
+      ) {
+        console.log(e);
+        throw new UserInputError(
+          "Unable to complete server setup contact support"
+        );
+      }
+      throw new UserInputError(e.message);
+    }
+  }
+
+  async StartServer(data) {
+    const { _id } = data;
+
+    let FoundServer = await __Server.findOne({ _id });
+
+    if (!FoundServer) {
+      throw new UserInputError("Unable to stop server");
+    }
+    const { host, username, pkey } = FoundServer;
+
+    await this.RemoteServer(host, username, pkey);
+
+    await ssh.execCommand(`dokku ps:start${host}`);
   }
 }
 
